02eea030ebf33fdff47c173a95b4cdf0131f44a2
aditiisingh311-tech <aditiisingh311@gmail.com>
2026-01-11 16:32:21 +0530

    GetStudentById,DeleteStudentById implemented

--- StudentManagementWebAPI/Controllers/StudentController.cs
+++ StudentManagementWebAPI/Controllers/StudentController.cs
@@ -30,5 +30,43 @@ namespace StudentManagementWebAPI.Controllers
                 return StatusCode(500, "Internal server error");
             }
         }
+        [HttpGet("GetStudentById/{id}")]
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            try
+            {
+                var student = _studentManagementDBContext.Students.Where(x=>x.Id==id);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                return Ok(student);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while fetching the student with ID {id}.");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        [HttpDelete("DeleteStudentByID/{id}")]
+        public ActionResult<Student> DeleteStudentById(int id)
+        {
+            try
+            {
+                var student = _studentManagementDBContext.Students.Find(id);
+                if (student == null)
+                {
+                    return NotFound();
+                }
+                _studentManagementDBContext.Students.Remove(student);
+                _studentManagementDBContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An error occurred while deleting the student with ID {id}.");
+                return StatusCode(500, "Internal server error");
+            }
+        }
     }
 }
